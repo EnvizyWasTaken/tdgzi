425f0a0b0fd4305fe70ab0fa412dda18af4db284
EnvizyWasTaken <keere922@gmail.com>
2026-03-21 21:57:07 +0000

    changed the name of PACKAGE_TYPE to PackageType to match its classification as an Enum (note: ues i love C and big letters)

--- src/rules.rs
+++ src/rules.rs
@@ -4,25 +4,25 @@ pub const INSTALLATION_SCRIPTS: &[&str] =
     &["install.sh", "Makefile", "CMakeLists.txt", "configure"];
 
 #[derive(Debug)]
-pub enum PACKAGE_TYPE {
+pub enum PackageType {
     Binary,
     Source,
     Script,
     Unknown,
 }
 
-pub fn classify(analysis: &ArchiveAnalysis) -> PACKAGE_TYPE {
+pub fn classify(analysis: &ArchiveAnalysis) -> PackageType {
     let has_script = analysis.executables.iter().any(|file| {
         INSTALLATION_SCRIPTS.iter().any(|script| file.contains(script))
     });
 
     if has_script {
-        PACKAGE_TYPE::Script
+        PackageType::Script
     } else if analysis.has_makefile {
-        PACKAGE_TYPE::Source
+        PackageType::Source
     } else if !analysis.executables.is_empty() {
-        PACKAGE_TYPE::Binary
+        PackageType::Binary
     } else {
-        PACKAGE_TYPE::Unknown
+        PackageType::Unknown
     }
 }
