a2537c76277e929d1794883cdc0df5d8b043e369
EnvizyWasTaken <keere922@gmail.com>
2026-03-21 21:55:32 +0000

    changed PACKAGE_TYPE to PackageType to match its classification as an enum (note: ues i do like C and big letters)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod installer;
+mod rules;
+mod scan;
+
+use clap::{Parser, Subcommand};
+use crate::rules::PackageType;
+
+#[derive(Parser)]
+#[command(name = "tdgzi")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Inspect { file: String },
+}
+
 fn main() {
     println!("Hello, world!");
 }
